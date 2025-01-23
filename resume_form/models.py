77c86f9395ce657f80c6d1bc094e2595d381dc4b
Davi Duarte <93225780+davidonigit@users.noreply.github.com>
2025-01-23 16:23:57 -0300

    html and css improvements

--- resume_form/models.py
+++ resume_form/models.py
@@ -7,17 +7,17 @@ def validate_file_size(file):
         raise ValidationError("O arquivo deve ter no máximo 1MB.")
 
 class ResumeSubmit(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
     celphone = models.CharField(max_length=15)
-    position = models.CharField(max_length=100)
+    position = models.CharField(max_length=50)
     education = models.CharField(max_length=50, choices=[
         ('fundamental', 'Ensino Fundamental'),
         ('medio', 'Ensino Médio'),
         ('superior', 'Ensino Superior'),
         ('pos', 'Pós-graduação')
     ])
-    observations = models.TextField(blank=True)
+    observations = models.TextField(blank=True, max_length=100)
     file = models.FileField(upload_to='curriculos/', validators=[validate_file_size])
     date = models.DateTimeField(auto_now_add=True)
     ip = models.GenericIPAddressField()
